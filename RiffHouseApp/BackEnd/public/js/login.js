45fc54bc365322073c8c23702608d99d15a8530b
lucagalli123 <galliluca_95@hotmail.com>
2025-11-22 11:01:56 -0300

    agrego validaciones de formulario de login y generales en utils.js, agrego mensaje de error en inputs, y estilos para inputs y mensajes de error

--- RiffHouseApp/BackEnd/public/js/login.js
+++ RiffHouseApp/BackEnd/public/js/login.js
@@ -1,7 +1,24 @@
-import { $ } from "./utils.js";
+import { $, limpiarError, listenersInputsLimpiarErrores } from "./utils.js";
+import { marcarError } from "./utils.js";
+
+import { listenersInputsBlur } from "./utils.js";
+import { listenersInputsFocus } from "./utils.js";
 
 const form = document.getElementById("loginForm");
 const btnAccesoRapido = document.getElementById("btnAccesoRapido");
+
+let inputEmail = $("inputEmail");
+let errorEmail = $("errorEmail");
+
+let inputPassword = $("inputPassword");
+let errorPassword = $("errorPassword");
+
+const listaInputsErrors = [
+    { input: $("inputEmail"), errorText: $("errorEmail") },
+    { input: $("inputPassword"), errorText: $("errorPassword") },
+];
+
+// funciones
 
 async function loguearse(email, password) {
     try {
@@ -26,14 +43,50 @@ async function loguearse(email, password) {
     }
 }
 
+function validarDatos(email, password) {
+    let todoOk = true;
+
+    if (!email) {
+        todoOk = false;
+        marcarError(inputEmail, errorEmail, "*Campo vacio");
+    } else {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            todoOk = false;
+            marcarError(inputEmail, errorEmail, "*El email no tiene un formato valido");
+        }
+    }
+
+    if (!password) {
+        todoOk = false;
+        marcarError(inputPassword, errorPassword, "*Campo vacio");
+    }
+
+    if (password.length < 1) {
+        todoOk = false;
+        marcarError(inputPassword, errorPassword, "*Campo vacio");
+    }
+    return todoOk;
+}
+
+// listeners
+
 form.addEventListener("submit", async e => {
     e.preventDefault();
     const emailValue = $("inputEmail").value;
     const passwordValue = $("inputPassword").value;
-    await loguearse(emailValue, passwordValue);
+    if (validarDatos(emailValue, passwordValue)) await loguearse(emailValue, passwordValue);
 });
 
+// listeners de los inputs y mensajes de error (blur, focus, input)
+listenersInputsBlur(listaInputsErrors);
+listenersInputsFocus(listaInputsErrors);
+listenersInputsLimpiarErrores(listaInputsErrors);
+
+// listener del boton de acceso rapido
 btnAccesoRapido.addEventListener("click", async () => {
+    limpiarError(inputEmail, errorEmail);
+    limpiarError(inputPassword, errorPassword);
     $("inputEmail").value = "moni@example.com";
     $("inputPassword").value = "1234";
 });
--- RiffHouseApp/BackEnd/public/js/utils.js
+++ RiffHouseApp/BackEnd/public/js/utils.js
@@ -1,3 +1,53 @@
 export function $(id) {
     return document.getElementById(id);
 }
+
+export function marcarError(input, errorText, errorMensaje) {
+    input.className = "";
+    input.classList.add("input-error");
+    errorText.textContent = errorMensaje;
+    errorText.classList.replace("oculto", "visible");
+}
+
+export function limpiarError(input, errorText) {
+    input.className = "";
+    input.classList.add("input-normal");
+    input.classList.add("input-focus");
+    errorText.classList.replace("visible", "oculto");
+}
+
+function inputFocus(input) {
+    input.className = "";
+    input.classList.add("input-normal");
+    input.classList.add("input-focus");
+}
+
+function inputBlur(input) {
+    input.className = "";
+    input.classList.add("input-normal");
+    input.classList.add("input-blur");
+}
+
+export function listenersInputsFocus(listaInputs) {
+    listaInputs.forEach(e => {
+        e.input.addEventListener("focus", () => {
+            if (!e.input.classList.contains("input-error")) inputFocus(e.input);
+        });
+    });
+}
+
+export function listenersInputsBlur(listaInputs) {
+    listaInputs.forEach(e => {
+        e.input.addEventListener("blur", () => {
+            if (!e.input.classList.contains("input-error")) inputBlur(e.input);
+        });
+    });
+}
+
+export function listenersInputsLimpiarErrores(listaInputs) {
+    listaInputs.forEach(e => {
+        e.input.addEventListener("input", () => {
+            limpiarError(e.input, e.errorText);
+        });
+    });
+}
